3d2d34b2d75e49f76e8998b336f58e9a66cab635
eukelios <Sven.Nueesch@kftg.ch>
2022-02-22 18:45:07 +0100

    Adapted to new socket.io version and merged with servo kick

--- node-servo/public/servo.js
+++ node-servo/public/servo.js
@@ -1,14 +1,39 @@
 /*
 * servo.js
-* client side servo control example
+* client side robot arm servo control example
 */
  
+var socket = io();                                  	//load socket.io-client and connect to the host that serves the page
+var sSlider;
+var fPulseLen;
+ 
 function initialize() {
-    var socket = io();                                  //load socket.io-client and connect to the host that serves the page
-    var sSlider = document.getElementById("servoSlider");
-    var angle = 0;
+
+	sSlider = document.getElementById("servoSlider");
+	fPulseLen = document.getElementById("pulseLen");
+	
     sSlider.addEventListener("input", function() {      //add event listener for when servo slider changes
-        angle = this.value;                             //update the angle value according to the slider
-        socket.emit("servoAngle", angle);               //send the current angle via WebSocket
+        plen = this.value;                              //update the angle value according to the slider
+		fPulseLen.innerHTML = plen;						//show the value to the user
+        socket.emit("pulseLen", plen);                  //send the current angle via WebSocket
     });
+	
+    var sSwitch = document.getElementById("servoSwitch");
+    sSwitch.addEventListener("input", function() {      //add event listener for when servo switch changes
+        sw = this.checked;                              //update the angle value according to the slider
+		if (sw == true) {
+			plen = 800;
+		} else {
+			plen = 2000;
+		}
+		fPulseLen.innerHTML = plen;						//show the value to the user
+		sSlider.value = plen;
+        socket.emit("pulseLen", plen);                  //send the current angle via WebSocket
+    });
+	
+}
+
+function btnClicked(slot,plon,ploff) {						//buttons can call functions directly by using onclick
+	// alert("Klicked!"+" "+slot+" "+plon+" "+ploff);								//for debugging reasons only
+	socket.emit("kick", slot, plon, ploff);
 }
--- node-servo/web-servo.js
+++ node-servo/web-servo.js
@@ -1,18 +1,22 @@
 /*
 * web-servo.js
 *
-* node web server servo control example
-* (c)2019 by S. Nueesch KFTG
+* Node Web Server Servo Control Example
 *
+* History:
+* 1.3   22.02.22    NUE  addapted to new socket.io
+* 1.2   11.07.20    NUE  added servo kick function 
+*
+* (c)2019-22 by S. Nueesch KFTG
 */
 "use strict";
  
 // web stuff
-var express = require('express');
-var app = express();
-var http = require('http');
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {allowEIO3: true});
 
 var path = require('path');
 
@@ -37,17 +41,30 @@ var pwm;
 
 
 io.on('connection', function (socket) {                 // web Socket Connection
-    socket.on('servoAngle', function(data) {                    // get servo angle from client
-        var angle = parseInt(data);
-        console.log("Received angle: "+angle);
-        if (angle >= -100 && angle <= 100) {
-            var pulseLen = (angle+100)/200*1000+1000;                // convert angle to usecs
-            console.log("PulseLen: "+pulseLen);
-            pwm.setPulseLength(pwmSlot, pulseLen);              // tell servo at pwmSlot to move to position
+    socket.on('pulseLen', function(plen) {            // get servo angle from client
+        var pulseLen = parseInt(plen);
+        console.log("Received Pulse Length: "+pulseLen);
+        if (pulseLen >= 700 && pulseLen <= 2300) {
+            pwm.setPulseLength(pwmSlot, pulseLen);      // tell servo at pwmSlot to move to position
         }
     });
+	
+	socket.on('kick', async function(slot,plon,ploff) {
+        var theSlot = parseInt(slot);
+        var thePlOn = parseInt(plon);
+		var thePlOff = parseInt(ploff);
+    	console.log("Kick received!"+" "+theSlot+" "+thePlOn+" "+thePlOff);
+        pwm.setPulseLength(theSlot, thePlOn);      		// tell servo at pwmSlot to move to position
+  	  	console.log('Taking a break...');
+		await sleep(1000);
+        pwm.setPulseLength(theSlot, thePlOff);      	// tell servo at pwmSlot to move to position		
+	});
 });
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+    
 process.on('SIGINT', function () {                              // on ctrl+c
     console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
     pwm.dispose();
@@ -55,26 +72,18 @@ process.on('SIGINT', function () {                              // on ctrl+c
 });
 
 
-
+// ---------------
 // main 
+// ---------------
 pwm = new Pca9685Driver(options, function startLoop(err) {          // initialize PCA9685
     if (err) {
         console.error("Error initializing PCA9685");
         process.exit(-1);
+    } else {
+        server.listen(8080);                                    // now listen on http://10.0.1.3:8080
+        console.log("Server is now listening on port 8080 (Press ctrl-c to abort) ...");
     }
-//    http.listen(8080);                                              // listen to port 8080
-//    console.log("Now listening on port 8080 for servo data ...");
 });
 
-server.listen(8080);
 
 
-// var path = require('path');
-// var express = require ('express');
-
-// console.log('starting app.js ...');
-// var app = express();
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.listen(8080, function () {
-//    console.log('Now listening on port 8080 for servo data ...');
-// });
